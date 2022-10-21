0bf8b498eae1ef7247b00a87cecbae80e8ebae2b
3B117151 <114204306+3B117151@users.noreply.github.com>
2022-10-21 12:05:03 +0800

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,12 @@
+public class Test2{
+    public static void main (String[] args){
+		int i,j;
+		
+        for (i=0,j=0; (i<10)&&(j<10); i++,j--){
+			System.out.printf("%d%n",i);
+			System.out.printf("%d%n",j);
+		}
+        
+			System.out.printf("%d%n",i);
+    }
+}
---
+++ Test6.java
@@ -0,0 +1,18 @@
+public class Test6{
+    public static void main (String[] args){
+	for(int i=1;i<=6;i++){
+		int j=1;
+		for(int k=0;k<6;k++){
+		if (k<6-i){
+			System.out.print("*");
+		}
+		else {
+			System.out.print(j);
+			j=j+1;
+		}
+		System.out.println();
+		}
+		
+	}
+	}
+}	
