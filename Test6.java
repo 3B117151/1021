6d326d33b4b3cfcd3cddedf31fc5b6926ba86495
3B117151 <114204306+3B117151@users.noreply.github.com>
2022-10-28 08:33:38 +0800

    Update Test6.java

--- Test6.java
+++ Test6.java
@@ -3,16 +3,15 @@ public class Test6{
 	for(int i=1;i<=6;i++){
 		int j=1;
 		for(int k=0;k<6;k++){
-		if (k<6-i){
-			System.out.print("*");
-		}
-		else {
-			System.out.print(j);
-			j=j+1;
+			if (k<6-i){
+				System.out.print("*");
+			}
+			else {
+				System.out.print(j);
+				j=j+1;
+			}
 		}
 		System.out.println();
-		}
-		
 	}
 	}
 }	
